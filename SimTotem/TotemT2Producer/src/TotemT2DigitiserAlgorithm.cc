95a3d5a95dfe0721a09fc7062fcb32c3228fd023
Laurent Forthomme <laurent.forthomme@cern.ch>
2021-04-23 18:29:27 +0200

    Simu: digitiser skeletton

---
+++ SimTotem/TotemT2Producer/src/TotemT2DigitiserAlgorithm.cc
@@ -0,0 +1,14 @@
+#include "SimTotem/TotemT2Producer/interface/TotemT2DigitiserAlgorithm.h"
+
+TotemT2DigitiserAlgorithm::TotemT2DigitiserAlgorithm(const edm::ParameterSet& iConfig) {}
+
+TotemT2DigitiserAlgorithm::~TotemT2DigitiserAlgorithm() {}
+
+void TotemT2DigitiserAlgorithm::setRunConditions(const TotemGeometry& geom) { geom_ = &geom; }
+
+void TotemT2DigitiserAlgorithm::addHits(const std::vector<PCaloHit>& hits, int bx) {
+  auto& bx_hits = hits_vs_bx_[bx];
+  bx_hits.insert(bx_hits.end(), hits.begin(), hits.end());
+}
+
+void TotemT2DigitiserAlgorithm::reset() { hits_vs_bx_.clear(); }
