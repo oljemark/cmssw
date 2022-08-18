0fedb72570e84d77f3871b62fc1a216b8f604033
Fredrik <fredrik.oljemark@helsinki.fi>
2022-08-18 09:12:43 +0200

    T2 digiproducer parameter name corrected

--- SimPPS/TotemT2DigiProducer/python/totemT2DigiParameters_cff.py
+++ SimPPS/TotemT2DigiProducer/python/totemT2DigiParameters_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 totem_t2_digi_parameters = cms.PSet(
-    hitsProducer = cms.InputTag('g4SimHits:TotemHitsT2Scint'),
+    hitsCollection = cms.InputTag('g4SimHits:TotemHitsT2Scint'),
     algo = cms.string('trivial')
 )
