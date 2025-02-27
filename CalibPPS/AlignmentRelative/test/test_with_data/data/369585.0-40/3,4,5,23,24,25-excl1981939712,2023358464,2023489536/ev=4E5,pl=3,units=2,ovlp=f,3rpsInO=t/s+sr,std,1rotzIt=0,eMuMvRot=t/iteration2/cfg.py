2c0db5698c5978846b6b3e86ae8649547f3cd5ca
Fredrik <fredrik.oljemark@helsinki.fi>
2025-02-27 01:17:59 +0100

    Increased fit limits, Output files for 45:-300nr/+0fr with 2 planes excluded

--- CalibPPS/AlignmentRelative/test/test_with_data/data/369585.0-40/3,4,5,23,24,25-excl1981939712,2023358464,2023489536/ev=4E5,pl=3,units=2,ovlp=f,3rpsInO=t/s+sr,std,1rotzIt=0,eMuMvRot=t/iteration2/cfg.py
+++ CalibPPS/AlignmentRelative/test/test_with_data/data/369585.0-40/3,4,5,23,24,25-excl1981939712,2023358464,2023489536/ev=4E5,pl=3,units=2,ovlp=f,3rpsInO=t/s+sr,std,1rotzIt=0,eMuMvRot=t/iteration2/cfg.py
@@ -83,7 +83,7 @@ process.ppsStraightTrackAligner.excludePlanes = cms.vuint32(1981939712,202335846
 process.ppsStraightTrackAligner.z0 = -217000
 process.ppsStraightTrackAligner.horizontalOffsets = cms.vstring('3:-3.077', '23:-3.121')
 
-process.ppsStraightTrackAligner.maxResidualToSigma = 10
+process.ppsStraightTrackAligner.maxResidualToSigma = 20
 process.ppsStraightTrackAligner.minimumHitsPerProjectionPerRP = 3
 
 process.ppsStraightTrackAligner.removeImpossible = True
@@ -93,7 +93,7 @@ process.ppsStraightTrackAligner.requireAtLeast3PotsInOverlap = True
 process.ppsStraightTrackAligner.additionalAcceptedRPSets = ""
 
 process.ppsStraightTrackAligner.cutOnChiSqPerNdf = True
-process.ppsStraightTrackAligner.chiSqPerNdfCut = 50
+process.ppsStraightTrackAligner.chiSqPerNdfCut = 200
 
 process.ppsStraightTrackAligner.maxTrackAx = 0.5E-3
 process.ppsStraightTrackAligner.maxTrackAy = 0.5E-3
