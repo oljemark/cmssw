6c04f6b9e5cdf57a8de2e14881a0ccce83293897
Fredrik <fredrik.oljemark@helsinki.fi>
2025-05-28 08:09:21 +0200

    Try running both arms on top of real RP offsets (substituted already, should make no difference)

--- CalibPPS/AlignmentRelative/test/test_with_data/tb_template.py
+++ CalibPPS/AlignmentRelative/test/test_with_data/tb_template.py
@@ -35,7 +35,7 @@ $inputFiles
 )
 
 # geometry
-process.load("Geometry.VeryForwardGeometry.geometryRPFromDD_2022_cfi")
+process.load("Geometry.VeryForwardGeometry.geometryRPFromDD_2023D_cfi")
 process.ctppsGeometryESModule.buildMisalignedGeometry = cms.bool(True)
 del(process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles[-1])
 process.XMLIdealGeometryESSource_CTPPS.geomXMLFiles.append("$geometry/RP_Dist_Beam_Cent.xml")
